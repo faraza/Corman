62c5902e6e31c753fc96240070022a2bbd90ad84
Faraz Abidi <faraz.abidi@gmail.com>
2022-08-29 18:37:41 -0700

    Added Sharp image model. Now to add character to the background.

--- ImageModule/index.ts
+++ ImageModule/index.ts
@@ -1,4 +1,12 @@
+import sharp from 'sharp'
+import path from 'path'
+
 export function testBasicImageGen (): void{
     console.log("Testing basic image gen")
-    //TODO
+    duplicateImage('/Users/farazabidi/Documents/Corman/animation_assets/backgrounds/kings_chambers.png')
+}
+
+async function duplicateImage(pathToImage: string): Promise<void>{
+    const newPath = path.dirname(pathToImage) + "/" + path.parse(pathToImage).name + "_dup" + path.extname(pathToImage)
+    await sharp(pathToImage).toFile(newPath)
 }

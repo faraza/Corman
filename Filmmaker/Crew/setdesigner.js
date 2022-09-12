b36d2e68b57e5c9891872c9e53e2aa700c96c32e
Faraz Abidi <faraz.abidi@gmail.com>
2022-09-11 21:42:46 -0700

    Got it working w/ the basics.

--- Filmmaker/Crew/setdesigner.js
+++ Filmmaker/Crew/setdesigner.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,14 +33,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateImage = void 0;
-function generateImage(imagePrompt, fileOutputLocation) {
+const child = __importStar(require("child_process"));
+const secrets_1 = require("../../secrets");
+function generateImage(imagePrompt, _fileOutputLocation) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield _getDummyImageFromServer(imagePrompt, fileOutputLocation); //TODO: Actually call server        
+        const pythonScriptPath = './ImageGeneratorPython/client.py'; //TODO: Deal with file paths
+        const scriptCommand = "export STABILITY_KEY=" + secrets_1.stabilityKey + " ; python3 " + pythonScriptPath + " -W 512 -H 512 \"" + imagePrompt + "\"";
+        return new Promise((res, rej) => {
+            child.exec(scriptCommand, (error, stdout, stderr) => {
+                if (error) {
+                    console.log("Error: ", error);
+                    rej(error);
+                }
+                console.log("STDOUT: ", stdout);
+                console.log("STDERR: ", stderr);
+                res();
+            });
+        });
     });
 }
 exports.generateImage = generateImage;
-function _getDummyImageFromServer(prompt, fileOutputLocation) {
-    return new Promise((resolve, reject) => {
+function _getDummyImageFromServer(_prompt, _fileOutputLocation) {
+    return new Promise((resolve, _reject) => {
         setTimeout(() => {
             resolve();
         }, 3000);
@@ -26,7 +63,9 @@ function _getDummyImageFromServer(prompt, fileOutputLocation) {
 function _testGenerateImage() {
     return __awaiter(this, void 0, void 0, function* () {
         console.log("TestGenerateImage");
-        //TODO: Run python script
+        const prompt = "A flaming red sun";
+        yield generateImage(prompt, "");
+        console.log("Done running Test GenerateImage");
     });
 }
 _testGenerateImage();
--- Filmmaker/Crew/setdesigner.ts
+++ Filmmaker/Crew/setdesigner.ts
@@ -1,9 +1,26 @@
-export async function generateImage(imagePrompt: string, fileOutputLocation: string): Promise<void>{
-    await _getDummyImageFromServer(imagePrompt, fileOutputLocation) //TODO: Actually call server        
+import * as child from 'child_process'
+import { stabilityKey } from '../../secrets';
+
+export async function generateImage(imagePrompt: string, _fileOutputLocation: string): Promise<void>{    
+    const pythonScriptPath = './ImageGeneratorPython/client.py' //TODO: Deal with file paths
+    const scriptCommand = "export STABILITY_KEY=" + stabilityKey + " ; python3 " + pythonScriptPath + " -W 512 -H 512 \"" + imagePrompt + "\""
+    return new Promise((res, rej)=>{
+        child.exec(scriptCommand, (error, stdout: string, stderr: string) => {
+            if(error){
+                console.log("Error: ", error)
+                rej(error)
+            } 
+            
+            console.log("STDOUT: ", stdout);      
+            console.log("STDERR: ", stderr)
+            res()
+        });
+    })
+    
 }
 
-function _getDummyImageFromServer(prompt: string, fileOutputLocation: string): Promise<void>{
-    return new Promise((resolve, reject)=>{
+function _getDummyImageFromServer(_prompt: string, _fileOutputLocation: string): Promise<void>{
+    return new Promise((resolve, _reject)=>{
         setTimeout(()=>{
             resolve()
         }, 3000)
@@ -12,7 +29,9 @@ function _getDummyImageFromServer(prompt: string, fileOutputLocation: string): P
 
 async function _testGenerateImage(){
     console.log("TestGenerateImage")
-    //TODO: Run python script
+    const prompt = "A flaming red sun"
+    await generateImage(prompt, "")    
+    console.log("Done running Test GenerateImage")
 }
 
 _testGenerateImage()
